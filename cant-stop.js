1483b720c9811b0f97b54f969ce44958b7929de4
Zachary Vance <za3k@za3k.com>
2024-11-14 15:42:21 -0500

    map display

--- cant-stop.js
+++ cant-stop.js
@@ -33,13 +33,63 @@
 //    [ ] You vs. AI mode
 
 var sprites
+class Game {
+    constructor() {
+        this.players = 4
+        this.playerProgress = {}
+        this.playerColumnsComplete = {}
+        for (var i=0; i<this.players; i++) {
+            this.playerProgress[i] = {}
+            this.playerColumnsComplete[i] = 0
+        }
+        this.columnComplete = {}
+
+        this.markersAvailable = 3
+        this.markerProgress = {}
+        this.markersOn = {}
+
+        this.playerTurn = 0
+
+        //updateUI()
+    }
+
+    placeToken(row, col, name) {
+        const tile = $(`#map > :eq(${row}) > :eq(${col})`)
+        tile.append(sprites[name].make())
+    }
+
+    lock(player, number, pos) {
+        this.playerProgress[player][number] = pos
+    }
+
+    updateUI() {
+        $(".token").remove()
+
+        // Place marker beans in the corner
+        for (var i=0; i<this.markersAvailable; i++) this.placeToken(13, i, "x")
+
+        // Columns
+        const playerSprites = "ABCDE"
+        const columnStart = { 2:8, 3:9, 4:10, 5:11, 6:12, 7:13, 8:12, 9:11, 10:10, 11:9, 12:8 }
+        for (var c=2; c<=12; c++) {
+            const r = columnStart[c]
+            // Place tokens for player progress
+            for (var p=0; p<this.players; p++)
+                for (var i=0; i<(this.playerProgress[p][c]||0); i++)
+                    this.placeToken(r - this.playerProgress[p][c] + i + 1, c-2, playerSprites[p])
+            // Place marker bean
+            if (this.markerProgress[c]) this.placeToken(r - this.markerProgress[c] + 1, c-2, "x")
+        }
+
+    }
+}
+
 async function init() {
     const spriteNames = [
         "die1", "die2", "die3", "die4", "die5", "die6", " ", "^", "|", "v", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "o", "x", "A", "B", "C", "D", "E"
     ]
     sprites = await Sprites.loadAll("sprite64.png", 64, spriteNames)
     diceSprites = ["",sprites["die1"], sprites["die2"], sprites["die3"], sprites["die4"], sprites["die5"], sprites["die6"]]
-    playerSprites = [sprites["A"], sprites["B"], sprites["C"], sprites["D"], sprites["E"]]
 
     const map = [
         "     7     ",
@@ -62,11 +112,22 @@ async function init() {
         $("#map").append(r)
         row = [...row]
         row.forEach((symbol, colNum) => {
-            //const t = $(`<div class="tile"></div>`)
             t = sprites[symbol].make()
             r.append(t)
         })
     })
 }
 
-init()
+async function main() {
+    await init()
+    game = new Game()
+    game.lock(1, 5, 3)
+    game.lock(1, 12, 2)
+    game.lock(2, 5, 2)
+    game.lock(2, 7, 7)
+    game.markerProgress[3] = 3
+    game.markersAvailable--
+    game.updateUI()
+}
+
+main()

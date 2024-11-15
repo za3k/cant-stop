ed87a30d2e7b71e8da929f7d152e932e537e7861
Zachary Vance <za3k@za3k.com>
2024-11-14 21:42:11 -0500

    Rolls

--- cant-stop.js
+++ cant-stop.js
@@ -7,9 +7,9 @@
 //        [X] Drop shadow (?)
 //        [x] Red, green, blue, pink token
 //            #648fff ##785EF0 #DC267F ##FE6100 #FFB000
+//[x] Hook up UI board display to game state
 //[ ] Make dice roll area (6 dice grouped correctly, plus up to 6 action buttons)
 //[ ] Add post-move actions (roll again, stay)
-//[ ] Hook up UI board display to game state
 //[ ] Add game logic, make all actions work
 //    [ ] Advancing
 //    [ ] Dice filtering
@@ -32,7 +32,10 @@
 //    [ ] Dice rolling animation
 //    [ ] You vs. AI mode
 
+function roll() { return 1 + Math.floor(Math.random()*6) }
+
 var sprites
+const playerSprites = "ABCDE"
 class Game {
     constructor() {
         this.players = 4
@@ -47,10 +50,14 @@ class Game {
         this.markersAvailable = 3
         this.markerProgress = {}
         this.markersOn = {}
+        this.markerComplete = {}
 
         this.playerTurn = 0
+        this.rolled = false
 
         //updateUI()
+        $(".action.roll").on("click", () => { this.rollDice() })
+        $(".action.stop").on("click", () => { this.endTurn(false) })
     }
 
     placeToken(row, col, name) {
@@ -62,14 +69,76 @@ class Game {
         this.playerProgress[player][number] = pos
     }
 
+    endTurn(busted) {} // TODO
+
+    rollDice() {
+        const rollTime = 3000;
+
+        $("#action-area .action").hide()
+        setTimeout(() => {
+            this.rolled = true
+            this.updateUI()
+        }, rollTime)
+        for (var i=0; i<rollTime; i += 300)
+            setTimeout(() => {
+                this.setDice([roll(), roll(), roll(), roll()])
+            }, i)
+    }
+
+    canAdvance(player, columns) {
+        var markersNeeded = 0
+        for (var i =0; i<columns.length; i++) {
+            const c = columns[i]
+            if (this.markerComplete[c] || this.columnComplete[c]) return false
+            if (!this.markersOn[c]) markersNeeded++;
+        }
+        return markersNeeded <= this.markersAvailable
+    }
+
+    setDice(dice) {
+        const diceSprites = ["","die1", "die2", "die3", "die4", "die5", "die6"]
+        const setDiceOption = (je, dice) => {
+            console.log(je, dice)
+            for (var i=0; i<4; i++) {
+                je.find(`.die:nth-child(${i+1})`).empty().append(sprites[diceSprites[dice[i]]].make())
+            }
+            const sums = [dice[0] + dice[1], dice[2] + dice[3]]
+
+            const actionSection = je.find('div:nth-child(5)').empty()
+
+            var actions = []
+            var stuck = true
+            function addAdvance(s) {
+                const action = $(`<div class="action">`)
+                if (s.length == 1)
+                    action.text(`Advance on ${s[0]}`)
+                else
+                    action.text(`Advance on ${s[0]} & ${s[1]}`)
+                actionSection.append(action)
+                action.hide() // Initially hidden
+                stuck = false
+            }
+
+            if (this.canAdvance(this.playerTurn, sums)) {
+                addAdvance(sums)
+            } else {
+                if (this.canAdvance(this.playerTurn, [sums[0]])) addAdvance([sums[0]])
+                if (this.canAdvance(this.playerTurn, [sums[1]])) addAdvance([sums[1]])
+            }
+            if (stuck) setTimeout(() => {this.endTurn(true)}, 2000)
+        }
+        setDiceOption($(".dice-option:nth-child(1)"), [dice[0], dice[1], dice[2], dice[3]])
+        setDiceOption($(".dice-option:nth-child(2)"), [dice[0], dice[2], dice[1], dice[3]])
+        setDiceOption($(".dice-option:nth-child(3)"), [dice[0], dice[3], dice[1], dice[2]])
+    }
+
     updateUI() {
-        $(".token").remove()
+        $(".sprite .sprite").remove()
 
         // Place marker beans in the corner
         for (var i=0; i<this.markersAvailable; i++) this.placeToken(13, i, "x")
 
         // Columns
-        const playerSprites = "ABCDE"
         const columnStart = { 2:8, 3:9, 4:10, 5:11, 6:12, 7:13, 8:12, 9:11, 10:10, 11:9, 12:8 }
         for (var c=2; c<=12; c++) {
             const r = columnStart[c]
@@ -81,6 +150,15 @@ class Game {
             if (this.markerProgress[c]) this.placeToken(r - this.markerProgress[c] + 1, c-2, "x")
         }
 
+        $("#turn-info").text(`It's Player ${this.playerTurn+1}'s turn`)
+        const sprite = sprites[playerSprites[this.playerTurn]]
+        $("#turn-info").prepend(sprite.make()).append(sprite.make())
+        if (this.playerTurn == 0) $(".action").show()
+        else $(".action").hide()
+
+        if (this.rolled) $("#action-area .action").hide()
+        else $("#dice-area .action").remove()
+        if (this.markersAvailable == 3) $(".action.stop").hide()
     }
 }
 
@@ -89,7 +167,6 @@ async function init() {
         "die1", "die2", "die3", "die4", "die5", "die6", " ", "^", "|", "v", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "o", "x", "A", "B", "C", "D", "E"
     ]
     sprites = await Sprites.loadAll("sprite64.png", 64, spriteNames)
-    diceSprites = ["",sprites["die1"], sprites["die2"], sprites["die3"], sprites["die4"], sprites["die5"], sprites["die6"]]
 
     const map = [
         "     7     ",
@@ -116,6 +193,8 @@ async function init() {
             r.append(t)
         })
     })
+
+    $(".action").hide()
 }
 
 async function main() {
@@ -125,8 +204,12 @@ async function main() {
     game.lock(1, 12, 2)
     game.lock(2, 5, 2)
     game.lock(2, 7, 7)
-    game.markerProgress[3] = 3
+    game.markerProgress[7] = 3
+    game.markersOn[7] = true
     game.markersAvailable--
+    game.markersAvailable--
+    game.markersAvailable--
+
     game.updateUI()
 }
 
